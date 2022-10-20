3eadc9d40487aaaa213f0b13dbea81c6b3b88aa8
Silvia-Mutete <silvia.mutete@student.moringaschool.com>
2022-10-20 17:02:46 +0300

    chore:case create route

--- app/controllers/cases_controller.rb
+++ app/controllers/cases_controller.rb
@@ -1,8 +1,24 @@
 class CasesController < ApplicationController
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
 
     def index
         cases = Case.all
         render json: cases, status: :ok
+    end
+
+    def create
+        case = Case.create!(case_params)
+        render json: case, status: :created
+    end
+
+    private
+
+    def case_params
+        params.permit(:case_type, :title, :description, :location, :date , :image_url, :status)
+    end
+
+    def render_unprocessable_entity_response(exception)
+        render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
     end
 end
