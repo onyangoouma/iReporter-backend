3fa28623c32438563f2e4ee6fcdecd365f7cbff6
Kipleting <kipletingedwin4@gmail.com>
2022-10-20 13:02:12 +0300

    create a user model

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,45 @@
+class UsersController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound,with: :response_not_found
+
+   
+    def index 
+        render json:User.all
+    end 
+
+
+    # def show
+    #     user = User.find_by(id: session[:user_id])
+    #     render json: user
+    # end
+
+    def show
+        user = User.find(params[:id]) 
+        render json:user
+    end
+    
+
+    def create 
+        user = User.create!(user_params) 
+        if user.valid?
+            session[:user_id] = user.id 
+            render json:user 
+        else
+            render json: { errors: user.errors.full_messages },status: :unprocessable_entity
+
+        end
+    end
+
+
+    private
+
+    def response_not_found 
+        render json: {  error: "User not found" }, status: :not_found
+    end
+
+    private
+
+    def user_params 
+        params.permit(:email, :password) 
+    end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "📃 Seeding data..."
+
+User.create( name: 'Edwin Rotich', username: 'EdwinRotich', email: 'edwinrotich@gmail.com',password_digest: 'edwinrotich')
+User.create( name: 'John Kamau', username: 'JohnKamau', email: 'johnkamau@gmail.com',password_digest: 'johnkamau')
+User.create( name: 'Peter Ndegwa', username: 'PeterNdegwa', email: 'peterndegwa@gmail.com',password_digest: 'peterndegwa')
+User.create( name: 'James Situma', username: 'JamesSituma', email: 'jamessituma@gmail.com',password_digest: 'jamessituma')
+User.create( name: 'Harrison Ouma', username: 'HarrisonOuma', email: 'harrisonouma@gmail.com',password_digest: 'harrisonouma')
+
+
+puts "📃 Done Seeding ..."
