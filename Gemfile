6cfb81c9a8d438fe7d725822e68d561562ff7179
Silvia-Mutete <silvia.mutete@student.moringaschool.com>
2022-10-20 16:06:05 +0300

    docs:installed faker

--- Gemfile
+++ Gemfile
@@ -39,6 +39,10 @@ gem "bootsnap", require: false
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
+# Used to generate seed data
+# https://github.com/faker-ruby/faker
+gem "faker", "~> 2.18"
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,42 @@
 puts "🌱 Seeding spices..."
 
+Case.create(
+    case_type: "redflag",
+    title: "Bad roads",
+    description:
+    location: "-42.2078,98.33",
+    date:Faker::Date.between(from: '2021-08-21', to: '2022-08-21'),
+    image_url:[],
+    status: "Under-investigation"
+)
 
+Case.create(
+    case_type: "redflag",
+    title: 'Leader tips street thugs'
+    description:
+    location: '-33.2078,18.023',
+    date: Faker::Date.between(from: '2021-08-21', to: '2022-08-21')
+    image_url:[],
+    status: "draft"
+)
+Case.create(
+    case_type: "intervention",
+    title: "Bridge contruction needed",
+    description:
+    location: "-42.2078,98.33",
+    date: Faker::Date.between(from: '2021-08-21', to: '2022-08-21'),
+    image_url:[],
+    status: "Under Investigation"
+)
+Case.create(
+    case_type: "redflag",
+    title: "Bad roads",
+    description:
+    location: '6.5922139, 3.3427375'
+    date:Faker::Date.between(from: '2021-08-21', to: '2022-08-21'),
+    image_url:[],
+    status: "Resolved"
+)
 
 
 puts "🌱 Done spices..."
