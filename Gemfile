648715e864cbf19200ce194b230ad3ec226254a5
Silvia-Mutete <silvia.mutete@student.moringaschool.com>
2022-10-26 15:31:17 +0300

    Fix: fixed bugs

--- Gemfile
+++ Gemfile
@@ -53,8 +53,8 @@ gem 'puma', '~> 5.0'
 # Use Active Model has_secure_password
 gem 'bcrypt', '~> 3.1.7'
 # Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin AJAX possible
-gem 'rack-cors'
-gem "active_model_serializers", "~> 0.10.12"
+# gem 'rack-cors'
+# gem "active_model_serializers", "~> 0.10.12"
 
 
 group :development, :test do
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_20_103328) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_20_114914) do
   create_table "admins", force: :cascade do |t|
     t.string "username"
     t.string "password"
-    t.integer "cased_id"
-ActiveRecord::Schema[7.0].define(version: 2022_10_26_070332) do
+    t.integer "incident_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "incidents", force: :cascade do |t|
     t.string "incident_type"
     t.string "title"
@@ -28,7 +31,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_26_070332) do
     t.datetime "updated_at", null: false
   end
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_20_092612) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "username"
